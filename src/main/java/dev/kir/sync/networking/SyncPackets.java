8ee6283b4ea353fa4d9382187655b2c38d98430f
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-11-11 18:52:14 -0800

    Fix dedicated servers not working

--- src/main/java/dev/kir/sync/networking/SyncPackets.java
+++ src/main/java/dev/kir/sync/networking/SyncPackets.java
@@ -9,25 +9,22 @@ import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 
 public final class SyncPackets {
     public static void init() {
+        PayloadTypeRegistry.playS2C().register(ShellUpdatePacket.ID, ShellUpdatePacket.CODEC);
+        PayloadTypeRegistry.playS2C().register(ShellStateUpdatePacket.ID, ShellStateUpdatePacket.CODEC);
+        PayloadTypeRegistry.playS2C().register(SynchronizationResponsePacket.ID, SynchronizationResponsePacket.CODEC);
+        PayloadTypeRegistry.playS2C().register(PlayerIsAlivePacket.ID, PlayerIsAlivePacket.CODEC);
+        PayloadTypeRegistry.playS2C().register(ShellDestroyedPacket.ID, ShellDestroyedPacket.CODEC);
+
         PayloadTypeRegistry.playC2S().register(SynchronizationRequestPacket.ID, SynchronizationRequestPacket.CODEC);
         ServerPlayNetworking.registerGlobalReceiver(SynchronizationRequestPacket.ID, (payload, context) -> payload.execute(context.player(), context.responseSender()));
     }
 
     @Environment(EnvType.CLIENT)
     public static void initClient() {
-        PayloadTypeRegistry.playS2C().register(ShellUpdatePacket.ID, ShellUpdatePacket.CODEC);
         ClientPlayNetworking.registerGlobalReceiver(ShellUpdatePacket.ID, (payload, context) -> payload.execute(context.player()));
-
-        PayloadTypeRegistry.playS2C().register(ShellStateUpdatePacket.ID, ShellStateUpdatePacket.CODEC);
         ClientPlayNetworking.registerGlobalReceiver(ShellStateUpdatePacket.ID, (payload, context) -> payload.execute(context.player()));
-
-        PayloadTypeRegistry.playS2C().register(SynchronizationResponsePacket.ID, SynchronizationResponsePacket.CODEC);
         ClientPlayNetworking.registerGlobalReceiver(SynchronizationResponsePacket.ID, (payload, context) -> payload.execute(context.player()));
-
-        PayloadTypeRegistry.playS2C().register(PlayerIsAlivePacket.ID, PlayerIsAlivePacket.CODEC);
         ClientPlayNetworking.registerGlobalReceiver(PlayerIsAlivePacket.ID, ((payload, context) -> payload.apply(context.player())));
-
-        PayloadTypeRegistry.playS2C().register(ShellDestroyedPacket.ID, ShellDestroyedPacket.CODEC);
         ClientPlayNetworking.registerGlobalReceiver(ShellDestroyedPacket.ID, ((payload, context) -> payload.apply(context.player())));
     }
 }
